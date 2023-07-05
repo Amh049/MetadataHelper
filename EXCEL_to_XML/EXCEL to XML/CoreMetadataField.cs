832c58c736e8578dd1d08bb720cc415f2d59758f
Amh049 <ashlyn.haynes@yahoo.com>
2023-07-05 18:31:02 -0500

    remove duplicated code

--- EXCEL_to_XML/EXCEL to XML/CoreMetadataField.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EXCEL_to_XML;
-
-public class CoreMetadataField
-{
-    public string Field { get; set; }
-    public string Value { get; set; }
-    public string Description { get; set; }
-}
